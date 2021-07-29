b3b026fa690db4f385c3ff23c4983812dd20eac7
julianegoe <goersch.juliane@dpa.com>
2021-07-30 01:02:39 +0200

    WIP auth

--- components/Chat.js
+++ components/Chat.js
@@ -2,68 +2,102 @@ import { Bubble, GiftedChat } from 'react-native-gifted-chat';
 import { KeyboardAvoidingView, Platform, View } from 'react-native';
 import React, { useCallback, useEffect, useState } from 'react';
 
-/* const firebase = require('firebase');
+import useConstructor from '../customHooks';
+
+const firebase = require('firebase');
 require('firebase/firestore');
 
-const useConstructor = (callBack = () => {}) => {
-	const [hasBeenCalled, setHasBeenCalled] = useRef(false);
-	if (hasBeenCalled) return;
-	callBack();
-	setHasBeenCalled(true);
-}; */
+if (!firebase.apps.length) {
+	firebase.initializeApp({
+		apiKey: 'AIzaSyBdzYEUB_CZgyask3dZACUXs9csz_ItYs0',
+		authDomain: 'chattyapp-863cc.firebaseapp.com',
+		projectId: 'chattyapp-863cc',
+		storageBucket: 'chattyapp-863cc.appspot.com',
+		messagingSenderId: '116642040466',
+		appId: '1:116642040466:web:cb892ce032a1e052c0744b',
+		measurementId: 'G-7H3EJSCR30',
+	});
+}
 
 export default function Chat(props) {
 	const { name, color } = props.route.params;
 	const [messages, setMessages] = useState([]);
-
-	/* useConstructor(() => {
-		if (!firebase.apps.length) {
-			firebase.initializeApp({
-				apiKey: 'AIzaSyBdzYEUB_CZgyask3dZACUXs9csz_ItYs0',
-				authDomain: 'chattyapp-863cc.firebaseapp.com',
-				projectId: 'chattyapp-863cc',
-				storageBucket: 'chattyapp-863cc.appspot.com',
-				messagingSenderId: '116642040466',
-				appId: '1:116642040466:web:cb892ce032a1e052c0744b',
-				measurementId: 'G-7H3EJSCR30',
-			});
-		}
-	}); */
-
-	/* 	useEffect(() => {
-		referenceChatMessages = firebase.firestore().collection('messages');
-		const unsubscribe = referenceShoppingLists.onSnapshot(onCollectionUpdate);
-	}, []); */
+	const [uid, setUid] = useState('');
+	const reference = firebase.firestore().collection('messages');
 
 	// sets name prop as title and initializies first messages
+	// gets executed whenever there is change in the firebase databasae
 	useEffect(() => {
+		let unsubscribeMessages;
+		let authUnsubscribe;
+		// set name as title of the Chat screen
 		props.navigation.setOptions({ title: name });
-		setMessages([
-			{
-				_id: 1,
-				text: 'Hello developer',
-				createdAt: new Date(),
-				user: {
-					_id: 2,
-					name: 'React Native',
-					avatar: 'https://placeimg.com/140/140/any',
-				},
-			},
-			{
-				_id: 2,
-				text: `${name} has entered the chat`,
-				createdAt: new Date(),
-				system: true,
-			},
-		]);
+
+		// wait for changes in user auth state
+		authUnsubscribe = firebase.auth().onAuthStateChanged(async (user) => {
+			if (!user) {
+				console.log('auth...');
+				await firebase.auth().signInAnonymously();
+			}
+
+			//update user state with currently active user data
+			setUid(user.uid);
+			console.log(user.uid);
+		});
+
+		// listen to changes in the data base
+		unsubscribeMessages = reference
+			.orderBy('createdAt', 'desc')
+			.onSnapshot(onCollectionUpdate, (err) => {
+				console.log('error: ' + err);
+			});
+
+		return () => {
+			unsubscribeMessages();
+			authUnsubscribe();
+		};
 	}, []);
 
 	// appends new messages to messages object so it can be displayed
-	const onSend = useCallback((messages = []) => {
-		setMessages((previousMessages) =>
-			GiftedChat.append(previousMessages, messages)
-		);
-	}, []);
+	const onSend = (newMessages = []) => {
+		addMessage(newMessages[0]);
+	};
+
+	// callback function for onSnapshot to listen to changes in darabase
+	const onCollectionUpdate = (querySnapshot) => {
+		const messages = [];
+
+		if (querySnapshot) {
+			// go through each document
+			querySnapshot.forEach((doc) => {
+				// get the QueryDocumentSnapshot's data
+				var data = doc.data();
+				messages.push({
+					_id: data._id,
+					text: data.text,
+					createdAt: data.createdAt.toDate(),
+					user: {
+						_id: data.user._id,
+						name: data.user.name,
+						avatar: data.user.avatar,
+					},
+				});
+			});
+			setMessages(messages);
+		} else return;
+	};
+
+	const addMessage = (message) => {
+		reference.add({
+			_id: message._id,
+			text: message.text,
+			createdAt: message.createdAt,
+			user: {
+				_id: uid,
+				name: name,
+			},
+		});
+	};
 
 	return (
 		/* Prevents Keyboard from hiding text input on Android phones */
@@ -75,7 +109,16 @@ export default function Chat(props) {
 			{/* Whole Chat UI out of the box */}
 			<GiftedChat
 				messages={messages}
-				onSend={(messages) => onSend(messages)}
+				user={{
+					_id: uid,
+					name: name,
+				}}
+				onSend={(currentMessage) => {
+					onSend(currentMessage);
+				}}
+				renderUsernameOnMessage={true}
+				showUserAvatar={false}
+				showAvatarForEveryMessage={true}
 				/* customizes color of speech bubble */
 				renderBubble={(props) => {
 					return (
@@ -85,7 +128,11 @@ export default function Chat(props) {
 								right: {
 									backgroundColor: '#A4C86A',
 								},
+								left: {
+									color: 'black',
+								},
 							}}
+							position={props.currentMessage.user._id == uid ? 'right' : 'left'}
 						/>
 					);
 				}}
---
+++ customHooks.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+
+const useConstructor = (callBack = () => {}) => {
+	const [hasBeenCalled, setHasBeenCalled] = useState(false);
+	if (hasBeenCalled) return;
+	callBack();
+	setHasBeenCalled(true);
+};
+
+export default useConstructor;
