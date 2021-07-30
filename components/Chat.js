b0692a7f3ade5f8f4a08fa9da8de4bcb012b7e3c
julianegoe <goersch.juliane@dpa.com>
2021-07-30 11:25:55 +0200

    refactored code to be more readable

--- components/Chat.js
+++ components/Chat.js
@@ -1,65 +1,48 @@
 import { Bubble, GiftedChat } from 'react-native-gifted-chat';
 import { KeyboardAvoidingView, Platform, View } from 'react-native';
 import React, { useCallback, useEffect, useState } from 'react';
-
-import useConstructor from '../customHooks';
-
-const firebase = require('firebase');
-require('firebase/firestore');
-
-if (!firebase.apps.length) {
-	firebase.initializeApp({
-		apiKey: 'AIzaSyBdzYEUB_CZgyask3dZACUXs9csz_ItYs0',
-		authDomain: 'chattyapp-863cc.firebaseapp.com',
-		projectId: 'chattyapp-863cc',
-		storageBucket: 'chattyapp-863cc.appspot.com',
-		messagingSenderId: '116642040466',
-		appId: '1:116642040466:web:cb892ce032a1e052c0744b',
-		measurementId: 'G-7H3EJSCR30',
-	});
-}
+import { auth, db } from '../firebaseConfig';
 
 export default function Chat(props) {
 	const { name, color } = props.route.params;
 	const [messages, setMessages] = useState([]);
 	const [uid, setUid] = useState('');
-	const reference = firebase.firestore().collection('messages');
 
-	// sets name prop as title and initializies first messages
-	// gets executed whenever there is change in the firebase databasae
+	// create a reference to the messages collection in firestore
+	const messagesReference = db.collection('messages');
+
 	useEffect(() => {
 		let unsubscribeMessages;
 		let authUnsubscribe;
 		// set name as title of the Chat screen
 		props.navigation.setOptions({ title: name });
 
-		// wait for changes in user auth state
-		authUnsubscribe = firebase.auth().onAuthStateChanged(async (user) => {
+		// authenticates user and waits for changes in user auth state
+		authUnsubscribe = auth.onAuthStateChanged(async (user) => {
 			if (!user) {
 				console.log('auth...');
-				await firebase.auth().signInAnonymously();
+				await auth.signInAnonymously();
 			}
 
 			//update user state with currently active user data
 			setUid(user.uid);
 			console.log(user.uid);
+
+			// listen to changes in the data base to update messages state
+			unsubscribeMessages = messagesReference
+				.orderBy('createdAt', 'desc')
+				.onSnapshot(handleCollectionUpdate);
 		});
 
-		// listen to changes in the data base
-		unsubscribeMessages = reference
-			.orderBy('createdAt', 'desc')
-			.onSnapshot(onCollectionUpdate, (err) => {
-				console.log('error: ' + err);
-			});
-
+		// cleanup function to unsubscribe from auth and database update when component is not mounted
 		return () => {
 			unsubscribeMessages();
 			authUnsubscribe();
 		};
 	}, []);
 
-	// callback function for onSnapshot to listen to changes in darabase
-	const onCollectionUpdate = (querySnapshot) => {
+	// callback function for onSnapshot to update messages state
+	const handleCollectionUpdate = (querySnapshot) => {
 		const messages = [];
 
 		if (querySnapshot) {
@@ -79,32 +62,32 @@ export default function Chat(props) {
 	};
 
 	// appends new messages to messages object so it can be displayed
-	const onSend = (newMessages = []) => {
-		addMessage(newMessages[0]);
+	const handleSend = (currentMessage = []) => {
+		addMessage(currentMessage[0]);
 	};
 
-	// adds messages to firebase
-	const addMessage = ({ _id, createdAt, text, user }) => {
+	// adds messages to firestore
+	const addMessage = (currentMessage) => {
 		setMessages((previousMessages) =>
-			GiftedChat.append(previousMessages, {
-				_id,
-				createdAt,
-				text,
-				user: {
-					_id: uid,
-					name: name,
-				},
-			})
+			GiftedChat.append(previousMessages, currentMessage)
 		);
-		reference.add({
-			_id,
-			createdAt,
-			text: `${text} from ${uid}`,
-			user: {
-				_id: uid,
-				name: name,
-			},
-		});
+		messagesReference.add(currentMessage);
+	};
+
+	const renderBubble = (props) => {
+		return (
+			<Bubble
+				{...props}
+				wrapperStyle={{
+					right: {
+						backgroundColor: '#A4C86A',
+					},
+					left: {
+						color: 'black',
+					},
+				}}
+			/>
+		);
 	};
 
 	return (
@@ -116,34 +99,18 @@ export default function Chat(props) {
 
 			<GiftedChat
 				messages={messages}
+				/* custom user id */
 				user={{
 					_id: uid,
 					name: name,
 				}}
 				onSend={(currentMessage) => {
-					onSend(currentMessage);
+					handleSend(currentMessage);
 				}}
-				/* customizes color of speech bubble */
-				renderBubble={(props) => {
-					return (
-						<Bubble
-							{...props}
-							wrapperStyle={{
-								right: {
-									backgroundColor: '#A4C86A',
-								},
-								left: {
-									color: 'black',
-								},
-							}}
-							// renders bubble according to current user
-							/* position={props.currentMessage.user._id == uid ? 'right' : 'left'} */
-						/>
-					);
-				}}
-				user={{
-					_id: 1,
-				}}
+				/* customizes style of speech bubble */
+				renderBubble={renderBubble}
+				showAvatarForEveryMessage={true}
+				renderUsernameOnMessage={true}
 			/>
 		</View>
 	);
